c4d07152c3186d55cf4b82ea3d0bda2f81595b11
thuccv98 <thuccv98@gmail.com>
2020-07-17 17:06:20 +0700

    commit all the code from lesson 1 to lesson 12

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 
+var userRoute = require('./routes/user.route');
+
+var db = require('./db');
 var port = 3000;
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.get('/', function(request, response) {
-	response.send('<h1>Hello Coders.tokyo</h1>');
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+//Routes
+app.get('/', function(req, res) {
+	res.render('index', {
+		name: 'ATDC'
+	});
 });
+
+app.use('/users', userRoute);
 
 app.listen(port, function() {
 	console.log('Server listening on port ' + port);
