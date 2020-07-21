dc12430f3bf02423344a1b8850acd66d70e4d2cd
thuccv98 <thuccv98@gmail.com>
2020-07-21 20:54:19 +0700

    lesson 13-14 middleware and cookie

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
 
-var db = require('./db');
 var port = 3000;
 var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
 app.use(express.static('public'));
 //Routes
