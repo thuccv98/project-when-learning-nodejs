71723c6106e754122ffbc0e030ccbbfa61ce7ebd
thuccv98 <thuccv98@gmail.com>
2020-07-22 19:27:47 +0700

    authentication login

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+
+var authMiddleware = require('./middlewares/auth.middleware');
 
 var port = 3000;
 var app = express();
@@ -21,7 +24,8 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.use('/users', userRoute);
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth', authRoute);
 
 app.listen(port, function() {
 	console.log('Server listening on port ' + port);
