cb35daec2e24bcb1ccaa358c043c31c576b70beb
thuccv98 <thuccv98@gmail.com>
2020-07-31 11:08:40 +0700

    test mongoose

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,15 +1,21 @@
-var db = require('../db');
+// var db = require('../db');
+var Product = require('../models/product.model');
 
-module.exports.index = function(req, res) {
-    var page = parseInt(req.query.page) || 1;// n
-    var perPage = 8;// x
 
-    // var start = (page - 1) * perPage;
-    // var end = (page -1) * perPage + perPage;
+module.exports.index = async function(req, res) {
+    // var page = parseInt(req.query.page) || 1;// n
+    // var perPage = 8;// x
 
-    var drop = (page - 1) * perPage;
+    // // var start = (page - 1) * perPage;
+    // // var end = (page -1) * perPage + perPage;
+
+    // var drop = (page - 1) * perPage;
+    // res.render('products/index', {
+    //     //products: db.get('products').value().slice(start, end)
+    //     products: db.get('products').drop(drop).take(perPage).value()//use lodash lib with 2 method is: drop and take
+    // });
+    var products = await Product.find()
     res.render('products/index', {
-        //products: db.get('products').value().slice(start, end)
-        products: db.get('products').drop(drop).take(perPage).value()//use lodash lib with 2 method is: drop and take
+        products: products
     });
 };
